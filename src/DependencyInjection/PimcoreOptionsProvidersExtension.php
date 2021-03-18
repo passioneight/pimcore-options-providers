738df013b420ccfbfcb55e0b688f85f945ae28b6
passioneight <michael.cuenez@passioneight.at>
2021-03-18 17:03:44 +0100

    Fixes #1

--- src/DependencyInjection/PimcoreOptionsProvidersExtension.php
+++ src/DependencyInjection/PimcoreOptionsProvidersExtension.php
@@ -4,31 +4,18 @@ namespace Passioneight\Bundle\PimcoreOptionsProvidersBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
-use Passioneight\Bundle\PimcoreOptionsProvidersBundle\Constant\Configuration as Config;
 
-class PimcoreOptionsProvidersExtension extends ConfigurableExtension
+class PimcoreOptionsProvidersExtension extends Extension
 {
     /**
      * @inheritDoc
      * @throws \Exception
      */
-    protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('config.yml');
-
-        $this->populateContainer($mergedConfig, $container);
-    }
-
-    /**
-     * Populates the container in order to access the configuration later on, if needed.
-     * @param array $config
-     * @param ContainerBuilder $container
-     */
-    public function populateContainer(array $config, ContainerBuilder $container)
-    {
-        $container->setParameter(Config::ROOT, $config);
     }
 }
